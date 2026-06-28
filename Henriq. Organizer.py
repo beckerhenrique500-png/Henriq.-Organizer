ca2ffdd9017b6f799ff1a1667ee4445181721821
beckerhenrique500-png <beckerhenrique500@gmail.com>
2026-06-28 03:30:45 -0300

    Add files via upload

---
+++ Henriq. Organizer.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+from pathlib import Path
+
+def exists():
+    if os.path.exists(rf"{path1}\{file1.suffix[1:]}"):
+        return True
+    
+def exists2():
+    if os.path.exists(rf"{path1}\{file1.suffix[1:]}\{file1}"):
+        return True
+    
+def create():
+    if not exists():
+        os.mkdir(rf"{path1}\{file1.suffix[1:]}")
+    
+    if not exists2():
+        shutil.move(rf"{path1}\{file1}", rf"{path1}\{file1.suffix[1:]}")
+
+print("Henriq. Organizer by @henriqueosdev TIKTOK\n")
+
+path1 = input(r"Enter the folder path (or drag it): ")
+path2 = os.listdir(path1)
+
+for i in path2:
+    file1 = Path(i)
+    create()
+
+print("Done.")
